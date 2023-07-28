87cdc1281abd658ef76891d12cffd9edc003f10c
umidjonxabibullaev <umidjonuzumaki@gmail.com>
2023-07-28 13:12:18 +0500

    capshe

---
+++ main.js
@@ -0,0 +1,22 @@
+// let a= Number( prompt("a ni kiriting")) 1-masala
+// let b= Number( prompt("b  ni kiritng"))
+// console.log(` S = ${a*b} kv`);
+// console.log(`p = ${2*(a+b)}`);
+ 
+// let d= Number( prompt("d ni kiriting")) 2-masala
+//  const pi=3.14
+//  console.log( `L =  ${pi*d}`);
+//  console.log(`r = ${d/2}`);
+//  console.log(`S = ${pi*(d/2)**2}`);
+// let a= Number( prompt("a ni kiriting")) 3-masala
+//  console.log(`V = ${a**3}`); 
+//  console.log(`S = ${6*a**2} `);
+// let a= Number( prompt("a ni kiriting")) 4-masala
+// let b= Number( prompt("b  ni kiritng"))
+// let c= Number( prompt("c ni kiriting"))
+// console.log(`V = ${a*b*c}`);
+// console.log(`S = ${2*(a*b+b*c+a*c)}`);
+// let a= Number( prompt("a ni kiriting")) 5-masala
+// let b= Number( prompt("b  ni kiritng"))
+// console.log(`M = ${(a+b)/2}`);
+
